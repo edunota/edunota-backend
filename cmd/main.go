09e124a098eefc567ef66c7d4cf9d9d0c8a4d69c
kerim cetinbas <a.cetinbas@icloud.com>
2023-09-27 21:34:37 +0300

    cleanup

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"github.com/gofiber/fiber"
-)
-
 func main() {
 
-	 app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) {
-    c.SendString("Hello, World!")
-})
-
-    app.Listen(":3000")
 }
