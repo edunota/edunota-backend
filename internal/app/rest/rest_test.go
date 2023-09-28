8a2c5cca08852676a8ee7a9ba796a40a79a305d9
cercinc <caglar.cercinli@vrt.be>
2023-09-28 21:12:11 +0200

    small fix

--- internal/app/rest/rest_test.go
+++ internal/app/rest/rest_test.go
@@ -4,12 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
-
-type MockedContext struct {
-	mock.Mock
-}
 
 func TestGetMesssage(t *testing.T) {
 	message := GetMessage()
